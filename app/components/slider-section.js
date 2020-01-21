e294eeebe4cfafbbb83b7106297484196d6fb969
Neftali Quintero <nquintero@araca.com>
2020-01-21 12:19:25 -0500

    Updating slider component

--- app/components/slider-section.js
+++ app/components/slider-section.js
@@ -1,8 +1,17 @@
+/*
+  Defines the behaviour and properties of the component. Behaviors are typically defined using actions.
+  
+*/
 import Component from '@ember/component';
 
-
 export default Component.extend({
-  model() {
-    return ['Marie Curie', 'Mae Jemison', 'Albert Hofmann'];
-  }
+ 
+ actions: {
+   next() {
+     alert("Next clicked");
+   },
+   back() {
+    alert("Back clicked");
+   }
+ }
 });
